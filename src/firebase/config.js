0dc5f193e65e3893032ab77e0a53aa533efd1670
Facundo <113198148+FacundoBugallo@users.noreply.github.com>
2023-10-04 18:50:17 -0300

    fix()

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,6 @@
-import '../src/style.css';
 import { initializeApp } from 'firebase/app';
 import {getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import {v4} from 'uuid';
-
-const files = document.querySelector('#file');
-const form = document.querySelector('.submitFile');
-
 const firebaseConfig = {
   apiKey: "AIzaSyBT_6OYaJYnd4oYcS01X1KGWPygWDTSOxM",
   authDomain: "photosintheclouds.firebaseapp.com",
@@ -21,26 +16,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 
-async function upLoaded(file){
+export async function upLoaded(file){
   const storageRef = ref(storage, v4())
   await uploadBytes(storageRef, file)
   let url = await getDownloadURL(storageRef)
   return url
 }
-
-let file;
-
-files.addEventListener("change",(ev) => {
-  file = ev.target.files[0];
-});
-
-form.addEventListener("submit",async (ev) => {
-  ev.preventDefault();
-  try {
-    const result = await upLoaded(file);
-    console.log(result)
-  } catch (error) {
-    console.log(error);
-    alert('Fallo interno')
-  };
-});
--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 import './style.css'
+import { upLoaded } from './firebase/config'
+
+const files = document.querySelector('#file');
+const form = document.querySelector('.submitFile');
+
+
+let file;
+
+files.addEventListener("change",(ev) => {
+  file = ev.target.files[0];
+});
+
+form.addEventListener("submit",async (ev) => {
+  ev.preventDefault();
+  try {
+    const result = await upLoaded(file);
+    console.log(result)
+  } catch (error) {
+    console.log(error);
+    alert('Fallo interno')
+  };
+});
